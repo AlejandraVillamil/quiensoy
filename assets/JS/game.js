7414bc20744ab349a332562e12aa51bd8614354f
Andres Felipe Cardona <62449597+afcardona25@users.noreply.github.com>
2020-09-27 21:43:31 -0500

    almost done

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,12 +1,54 @@
+//creación de variables
 let nombres = ["Alejandra Villamil", "Andres Cardona", "Carlos Alvarez", "Alejandro Fandiño"]
+let respondidos = []
 let input = document.getElementById("respuestaInput").value
 let points = 0 
-let text = ["blah blah blah blah blah blahblah blah blahblah blah blah", "bleeeeee bleeeeee bleeeeee bleeeeee bleeeeee bleeeeee", "bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiiibliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii bliiiiiiiiii"]
+let textPista = {
+    alejandra: "Soy Ingeniera de diseño y automatización electrónica, trabajo como ingeniera de soporte  y soy una  apasionada por la pastelería",
+    andres: "Soy administrador de empresas y consultor para la gestión de crisis empresariales.  Me encanta cocinar y tocar guitarra",
+    carlos: "Le gusta programar,  ama el css y ademas fue asesorado por el pony de los 7 colores.",
+    alejandroF: "Soy una persona emprendedora, que adora los retos y no se rinde fácilmente. Matemático de profesión, detallista y autodidacta cada día, no dejo de aprender. Estoy interesado en las nuevas tecnologías y en las aplicaciones matemáticas en las distintas areas del conocimiento humano."
+}
+let random = 0
+let body = document.getElementsByTagName("body")
+let posicion = 0
+
+
+
+ //esta variable es solo para el random que obtiene la pista 
+ //sobre el tecto. cada una de las posiciones del array 
+ //corresponde con una key del array textPista
+
+
+
+const saveInStorage = () => {
+    const dataString = JSON.stringify(textPista)
+        localStorage.setItem('reminingNames', dataString)
+
+    
+
+}
+const savePoints = ()=>{
+    const pointsString = JSON.stringify(points)
+    localStorage.setItem('points', pointsString)
+}
+function onloadProcess(){
+    if (localStorage.getItem('reminingNames') == null) {
+        console.log("start")
+    } else {
+        textPista = JSON.parse(localStorage.getItem("reminingNames"))
+        points = JSON.parse(localStorage.getItem('points'))
+    }
+
+}
+body.onload = onloadProcess()
+
 let pista = document.getElementById("obtenerPista")
 pista.addEventListener('click', ()=>{
+    saveInStorage(textPista)
     console.log("click en pista")
     document.getElementById("pista").classList.remove("d-none")
-    document.getElementById("pista").innerHTML = text[2]
+    document.getElementById("pista").innerHTML = textPista[0]
 })
 
 for ( i = 0 ; i<4; i++){
@@ -17,28 +59,63 @@ function matchRespuesta(){
     if(n === true){
         
         swal("Bien", "Atinaste", "success")
-        points++
+        points += 1
+        savePoints()
         console.log(`Tiene ${points} puntos`)
         switch(input){
             case "Alejandra Villamil":
-                document.getElementById('answer'+0).innerText = input
+                posicion = 0
+                document.getElementById('answer'+posicion).innerText = input
+                personas.splice(0,1)
+                console.log(personas)
+                delete textPista.alejandra
+                
+                
                 break;
             case "Andres Cardona":
-                document.getElementById('answer' + 1).innerText = input
+                posicion = 1
+                document.getElementById('answer' + posicion).innerText = input
+                personas.splice(1, 1)
+                borrarPistasInutiles("andres")
+                console.log(personas)
+                delete textPista.andres
+                
+                
                 break;
             case "Carlos Alvarez":
-                document.getElementById('answer' + 2).innerText = input
+                posicion = 2
+                document.getElementById('answer' + posicion).innerText = input
+                personas.splice(2, 1)
+                borrarPistasInutiles("carlos")
+                console.log(personas)
+                delete textPista.carlos
+                
                 break;
             case "Alejandro Fandiño":
-                document.getElementById('answer' + 3).innerText = input
+                posicion = 3
+                document.getElementById('answer' + posicion).innerText = input
+                personas.splice()
+                borrarPistasInutiles("alejandroF")
+                console.log(personas)
+                delete textPista.alejandroF
+                
                 break;
         }
+        filledInfo = {
+            nombre: input,
+            posicion: posicion
+        }
+        saveInStorage()
+        saveInStorageFilledData(filledInfo)
+        
     } else{
-        points--
+        points -=1
+        savePoints()
         console.log(`Tiene ${points} puntos`)
         swal("Error!", "Sigue intentando", "error")
+        
     }
-    localStorage.setItem('points', points)
+    
 }
 let respuesta = document.getElementById("respuestaBtn")
 respuesta.addEventListener('click', ()=>{
@@ -53,4 +130,27 @@ respuesta.addEventListener('click', ()=>{
     }
     
 })
+const saveInStorageFilledData = (filledInfo) => {
+    if (localStorage.getItem('nombres') == null) {
+        respondidos.push(filledInfo)
+        const nombresString = JSON.stringify(respondidos)
+        localStorage.setItem('nombres', nombresString)
+    } else {
+        listInStorage = JSON.parse(localStorage.getItem("nombres"))
+        listInStorage.push(filledInfo)
+        const nombresString = JSON.stringify(listInStorage)
+        localStorage.setItem('nombres', nombresString)
+    }
+}
+const answersStored = () => {
+    const namesInStorage = JSON.parse(localStorage.getItem('nombres'))
+    
+    for (let i = 0; i < namesInStorage.length; i += 1) {
+        document.getElementById('answer' + namesInStorage[i].posicion).innerText = namesInStorage[i].nombre
+        
+    }
+}
+answersStored()
+
+
 
